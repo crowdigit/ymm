8776a1630b5d75957f4c677942b317377e7d97e4
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-04 14:13:22 +0900

    add parser interface and test

--- main.go
+++ main.go
@@ -1,6 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
+
+type Progress struct {
+	Percentage int
+}
+
+type ProgressMessage interface {
+	Update(*Progress)
+}
+
+type ProgressExtractor struct{}
+
+func (p ProgressExtractor) Update(*Progress) {
+}
+
+type ProgressDestination struct{}
+
+func (p ProgressDestination) Update(*Progress) {
+}
+
+type ProgressDownload struct {
+	percentage int
+}
+
+func (p ProgressDownload) Update(*Progress) {
+}
+
+type ProgressFFMPEGDestination struct{}
+
+func (p ProgressFFMPEGDestination) Update(*Progress) {
+}
+
+type ProgressFFMPEGDelete struct{}
+
+func (p ProgressFFMPEGDelete) Update(*Progress) {
+}
+
+func parseStream(read io.Reader) (chan ProgressMessage, error) {
+	chMessage := make(chan ProgressMessage)
+	defer close(chMessage)
+	return chMessage, nil
+}
 
 func main() {
 	fmt.Println("vim-go")
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseReader(t *testing.T) {
+	testInput, err := os.Open("./test.txt")
+	assert.Nil(t, err)
+	defer testInput.Close()
+
+	chMessage, err := parseStream(testInput)
+	assert.Nil(t, err)
+
+	progresses := make([]ProgressMessage, 0, 49)
+	for progress := range chMessage {
+		progresses = append(progresses, progress)
+	}
+
+	percentages := []int{0, 1, 2, 5, 10, 21, 43, 66, 90, 118, 140, 170, 198, 225, 249, 275, 300, 328, 350, 377, 403, 428, 459, 484, 507, 536, 564, 587, 614, 642, 669, 697, 721, 748, 769, 796, 821, 848, 872, 901, 929, 952, 979, 1000, 1000}
+	expected := make([]ProgressMessage, 0, 49)
+	expected = append(expected, ProgressExtractor{})
+	expected = append(expected, ProgressDestination{})
+	for _, percentage := range percentages {
+		expected = append(expected, ProgressDownload{
+			percentage: percentage,
+		})
+	}
+	expected = append(expected, ProgressFFMPEGDestination{})
+	expected = append(expected, ProgressFFMPEGDelete{})
+
+	assert.Equal(t, len(expected), len(progresses))
+	assert.Equal(t, expected, progresses)
+}
