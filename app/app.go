016e4310e0920aaecdd41ebdd6fd708a14897025
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-18 14:16:13 +0900

    chore: add dummy app implementation

--- app/app.go
+++ app/app.go
@@ -1,7 +1,25 @@
 package app
 
+import "github.com/crowdigit/ymm/ydl"
+
 //ge:generate mockgen -destination=../../mock/mock_app.go -package=mock github.com/crowdigit/ymm/app Application
 type Application interface {
 	DownloadPlaylist(url string)
 	DownloadSingle(url string)
+}
+
+type ApplicationImpl struct {
+	ydl ydl.YoutubeDL
+}
+
+func NewApplicationImpl(ydl ydl.YoutubeDL) Application {
+	return ApplicationImpl{}
+}
+
+func (app ApplicationImpl) DownloadPlaylist(url string) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (app ApplicationImpl) DownloadSingle(url string) {
+	panic("not implemented") // TODO: Implement
 }
