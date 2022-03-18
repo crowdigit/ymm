dbd641ad5a70d2072ac84c4ee23c04a630d5f3bb
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-18 14:40:14 +0900

    feature: return download result from ydl interface

--- ydl/youtubedl.go
+++ ydl/youtubedl.go
@@ -1,12 +1,13 @@
 package ydl
 
 type VideoMetadata struct{}
+type DownloadResult struct{}
 
 //go:generate mockgen -destination=../mock/mock_ydl.go -package=mock github.com/crowdigit/ymm/ydl YoutubeDL
 type YoutubeDL interface {
 	PlaylistMetadata(url string) ([]VideoMetadata, error)
 	VideoMetadata(url string) (VideoMetadata, error)
-	Download(url VideoMetadata) error
+	Download(url VideoMetadata) (DownloadResult, error)
 }
 
 type YoutubeDLImpl struct{}
@@ -23,6 +24,6 @@ func (ydl YoutubeDLImpl) VideoMetadata(url string) (VideoMetadata, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (ydl YoutubeDLImpl) Download(url VideoMetadata) error {
+func (ydl YoutubeDLImpl) Download(url VideoMetadata) (DownloadResult, error) {
 	panic("not implemented") // TODO: Implement
 }
