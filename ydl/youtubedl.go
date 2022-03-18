2ee8b6e64299db6e6da04decd23b292bd7566ce1
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-18 14:35:02 +0900

    feature: return error from ydl interface

--- ydl/youtubedl.go
+++ ydl/youtubedl.go
@@ -4,9 +4,9 @@ type VideoMetadata struct{}
 
 //go:generate mockgen -destination=../mock/mock_ydl.go -package=mock github.com/crowdigit/ymm/ydl YoutubeDL
 type YoutubeDL interface {
-	PlaylistMetadata(url string) []VideoMetadata
-	VideoMetadata(url string) VideoMetadata
-	Download(url VideoMetadata)
+	PlaylistMetadata(url string) ([]VideoMetadata, error)
+	VideoMetadata(url string) (VideoMetadata, error)
+	Download(url VideoMetadata) error
 }
 
 type YoutubeDLImpl struct{}
@@ -15,14 +15,14 @@ func NewYoutubeDLImpl() YoutubeDL {
 	return YoutubeDLImpl{}
 }
 
-func (ydl YoutubeDLImpl) PlaylistMetadata(url string) []VideoMetadata {
+func (ydl YoutubeDLImpl) PlaylistMetadata(url string) ([]VideoMetadata, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (ydl YoutubeDLImpl) VideoMetadata(url string) VideoMetadata {
+func (ydl YoutubeDLImpl) VideoMetadata(url string) (VideoMetadata, error) {
 	panic("not implemented") // TODO: Implement
 }
 
-func (ydl YoutubeDLImpl) Download(url VideoMetadata) {
+func (ydl YoutubeDLImpl) Download(url VideoMetadata) error {
 	panic("not implemented") // TODO: Implement
 }
