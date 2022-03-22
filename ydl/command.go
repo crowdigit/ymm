85a7773fc167209164e201894b7fe30356ea5bf1
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-22 12:35:38 +0900

    feature: implement command provider

--- ydl/command.go
+++ ydl/command.go
@@ -1,6 +1,9 @@
 package ydl
 
-import "io"
+import (
+	"io"
+	"os/exec"
+)
 
 //go:generate mockgen -destination=../mock/mock_command.go -package=mock github.com/crowdigit/ymm/ydl Command
 type Command interface {
@@ -13,4 +16,40 @@ type Command interface {
 //go:generate mockgen -destination=../mock/mock_command_provider.go -package=mock github.com/crowdigit/ymm/ydl CommandProvider
 type CommandProvider interface {
 	NewCommand(name string, arg ...string) Command
+}
+
+type commandImpl struct {
+	cmd *exec.Cmd
+}
+
+func (c commandImpl) Start() error {
+	return c.cmd.Start()
+}
+
+func (c commandImpl) StderrPipe() (io.ReadCloser, error) {
+	return c.cmd.StderrPipe()
+}
+
+func (c commandImpl) StdoutPipe() (io.ReadCloser, error) {
+	return c.cmd.StdoutPipe()
+}
+
+func (c commandImpl) Wait() (int, error) {
+	if err := c.cmd.Wait(); err != nil {
+		return 0, err
+	}
+	code := c.cmd.ProcessState.ExitCode()
+	return code, nil
+}
+
+type commandProviderImpl struct{}
+
+func (cp commandProviderImpl) NewCommand(name string, arg ...string) Command {
+	return commandImpl{
+		cmd: exec.Command(name, arg...),
+	}
+}
+
+func NewCommandProviderImpl() commandProviderImpl {
+	return commandProviderImpl{}
 }
