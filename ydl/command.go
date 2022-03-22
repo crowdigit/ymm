2ebaccfec413991142abfd6c049ab6339923f881
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-22 11:19:05 +0900

    refactor: add command interface and mock

--- ydl/command.go
+++ ydl/command.go
@@ -2,10 +2,15 @@ package ydl
 
 import "io"
 
-//go:generate mockgen -destination=../mock/mock_command.go -package=mock github.com/crowdigit/ymm/ydl CommandProvider
-type CommandProvider interface {
-	Start(name string, arg ...string) error
+//go:generate mockgen -destination=../mock/mock_command.go -package=mock github.com/crowdigit/ymm/ydl Command
+type Command interface {
+	Start() error
 	StderrPipe() (io.ReadCloser, error)
 	StdoutPipe() (io.ReadCloser, error)
 	Wait() (int, error)
+}
+
+//go:generate mockgen -destination=../mock/mock_command_provider.go -package=mock github.com/crowdigit/ymm/ydl CommandProvider
+type CommandProvider interface {
+	NewCommand(name string, arg ...string) Command
 }
