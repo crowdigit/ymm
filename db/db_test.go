ac23c5967b0cfd661c25b9f70d3e1f011031d34f
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-28 11:05:53 +0900

    test: add DB test

---
+++ db/db_test.go
@@ -0,0 +1,62 @@
+package db_test
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/crowdigit/ymm/db"
+	"github.com/golang/mock/gomock"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/stretchr/testify/suite"
+)
+
+type DBTestSuite struct {
+	suite.Suite
+
+	mockCtrl *gomock.Controller
+	dataDir  string
+	config   db.DatabaseConfig
+}
+
+func (s *DBTestSuite) SetupTest() {
+	s.mockCtrl = gomock.NewController(s.T())
+	s.dataDir = s.T().TempDir()
+	s.config = db.DatabaseConfig{
+		DatabaseFile: filepath.Join(s.dataDir, "db.sql"),
+		MetadataDir:  filepath.Join(s.dataDir, "metadata"),
+	}
+}
+
+func (s *DBTestSuite) TearDownTest() {
+	s.mockCtrl.Finish()
+}
+
+func (s *DBTestSuite) TestStoreMetadata() {
+	id := "Ss-ba-g82-0"
+	expectedMap := make(map[string]any)
+	expectedMap["ID"] = id
+	expectedMap["a"] = 123
+	expectedMap["b"] = "asdf"
+	expected, err := jsoniter.Marshal(expectedMap)
+	s.Nil(err)
+
+	db := db.NewDatabaseImpl(s.config)
+	s.Nil(db.StoreMetadata(expected))
+
+	path := filepath.Join(s.config.MetadataDir, fmt.Sprintf("%s.json", id))
+	file, err := os.OpenFile(path, os.O_RDONLY, 0)
+	s.Nil(err)
+	defer file.Close()
+
+	got, err := io.ReadAll(file)
+	s.Nil(err)
+
+	s.Equal(expected, got)
+}
+
+func TestDBTestSuite(t *testing.T) {
+	suite.Run(t, new(DBTestSuite))
+}
