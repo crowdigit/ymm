627f50e6d242bb7118d0c0afbd4ae9cdf6c95b17
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-03-28 11:47:03 +0900

    feature: update example code

--- examples/main.go
+++ examples/main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/adrg/xdg"
+	"github.com/crowdigit/ymm/app"
+	"github.com/crowdigit/ymm/db"
 	"github.com/crowdigit/ymm/ydl"
 )
 
 func main() {
+	url := "https://www.youtube.com/watch?v=Ss-ba-g82-0"
+
 	commandProvider := ydl.NewCommandProviderImpl()
 	youtubeDl := ydl.NewYoutubeDLImpl(commandProvider)
 
-	url := "https://www.youtube.com/watch?v=Ss-ba-g82-0"
-	metadata, err := youtubeDl.VideoMetadata(url)
-	if err != nil {
-		log.Fatalf("failed to get video metadata: %s", err)
+	metadataDir := filepath.Join(xdg.DataHome, "ymm", "metadata")
+	if err := os.MkdirAll(metadataDir, 0755); err != nil {
+		log.Fatalf("failed to make metadata directory: %s", err)
 	}
 
-	log.Printf("%v", metadata)
+	databaseFile, err := xdg.DataFile("ymm/db.sql")
+	if err != nil {
+		log.Fatalf("failed to create data directory: %s", err)
+	}
+
+	db := db.NewDatabaseImpl(db.DatabaseConfig{
+		DatabaseFile: databaseFile,
+		MetadataDir:  metadataDir,
+	})
+
+	app := app.NewApplicationImpl(youtubeDl, db)
+	if err := app.DownloadSingle(url); err != nil {
+		log.Fatalf("%s", err)
+	}
 }
