8180f235ea4d01dc5babb7513f74e63a9b1d2a13
crowdigit <87285844+crowdigit@users.noreply.github.com>
2022-04-04 13:24:34 +0900

    feature: implement loudness scanner interface

--- loudness/loudness.go
+++ loudness/loudness.go
@@ -1,7 +1,11 @@
 package loudness
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/crowdigit/ymm/command"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
 
@@ -22,6 +26,64 @@ func NewLoudnessScanner(logger *zap.SugaredLogger, commandProvider command.Comma
 	}
 }
 
+func handleTagStream(logger *zap.SugaredLogger, chStdout <-chan []byte, chStderr <-chan []byte, chClose chan struct{}, chErr <-chan error) {
+loop:
+	for {
+		select {
+		case err := <-chErr:
+			logger.Errorf("reading from loudness scanner stream returned an error: %s", err)
+			break loop
+		case msg := <-chStderr:
+			logger.Warnf(string(msg))
+		case msg := <-chStdout:
+			logger.Infof(string(msg))
+		case <-chClose:
+			break loop
+		}
+	}
+}
+
 func (s LoudnessScannerImpl) Tag(path string) error {
-	panic("not implemented") // TODO: Implement
+	cmd := s.commandProvider.NewCommand("loudness", "tag", "--track", path)
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to get stderr pipe")
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to get stdout pipe")
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	chStderr := make(chan []byte)
+	chStdout := make(chan []byte)
+	chErr := make(chan error)
+	go command.ReadStream(&wg, stderr, chStderr, chErr)
+	go command.ReadStream(&wg, stdout, chStdout, chErr)
+
+	if err := cmd.Start(); err != nil {
+		return errors.Wrap(err, "failed to start loudness scanner command")
+	}
+
+	chClose := make(chan struct{})
+	go handleTagStream(s.logger, chStdout, chStderr, chClose, chErr)
+
+	wg.Wait()
+
+	close(chClose)
+
+	status, err := cmd.Wait()
+	if err != nil {
+		return errors.Wrap(err, "failed to wait for loudness scanner command")
+	}
+
+	if status != 0 {
+		return fmt.Errorf("loudness scanner command exited with %d", status)
+	}
+
+	return nil
 }
