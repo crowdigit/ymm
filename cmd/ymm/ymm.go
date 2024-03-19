b6af2ecc302f8abcca24f538dd8f1f3d56e534cf
crowdigit <87285844+crowdigit@users.noreply.github.com>
2024-03-19 17:25:47 +0900

    feature: make configurable

--- cmd/ymm/ymm.go
+++ cmd/ymm/ymm.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/adrg/xdg"
 	"github.com/crowdigit/exec"
 	"github.com/crowdigit/ymm/internal"
 	"github.com/spf13/cobra"
@@ -37,6 +39,14 @@ var singleCmd = &cobra.Command{
 }
 
 func init() {
+	configPath, err := xdg.ConfigFile(filepath.Join("ymm", "config.toml"))
+	if err != nil {
+		panic(err)
+	}
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(filepath.Dir(configPath))
+
 	viper.SetDefault("command.metadata.youtube.path", "yt-dlp")
 	viper.SetDefault("command.metadata.youtube.args", []string{"--dump-json", "<url>"})
 	viper.SetDefault("command.metadata.json.path", "jaq")
@@ -57,6 +67,15 @@ func init() {
 			"<url>",
 		},
 	)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			panic(fmt.Errorf("failed to read config file: %w", err))
+		}
+		if err := viper.SafeWriteConfig(); err != nil {
+			panic(fmt.Errorf("failed to write config file: %w", err))
+		}
+	}
 
 	rootCmd.AddCommand(singleCmd)
 }
